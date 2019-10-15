12e4fab0af465c83fd1cee96e270865cc461b61a
kl4532 <kornellukasiak@gmail.com>
2019-10-15 11:46:44 +0200

    cleanup

--- src/app/hamburger-menu/hamburger-menu.component.ts
+++ src/app/hamburger-menu/hamburger-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'hamburger-menu',
@@ -10,28 +10,35 @@ export class HamburgerMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if(typeof this.hcolor !== 'undefined'){
+      let el = (document.getElementsByClassName('hamburger')[0])as HTMLElement;
+      el.style.backgroundColor=(this.hcolor);
+    }
+    if(typeof this.hstripes !== 'undefined'){
+      for(let i = 1; i<5; i++){
+      document.getElementById('s'+ i).style.backgroundColor=(this.hstripes);
+      }
+    }
+    
   }
-  test(){
-    if(this.open){
-      document.getElementById("stripe2").style.transform = "rotate(-180deg)";
-      document.getElementById("stripe4").style.transform = "rotate(180deg)";
-      document.getElementById("stripe1").style.opacity = "1";
-      document.getElementById("stripe3").style.opacity = "1";
-      document.getElementById("menu").classList.toggle("visible", false);
+  @Input('hcolor') hcolor: string;
+  @Input('hstripes') hstripes: string;
 
+  toggle(){
+    if(this.open){
+      document.getElementById("s2").style.transform = "rotate(-180deg)";
+      document.getElementById("s3").style.transform = "rotate(180deg)";
+      document.getElementById("s1").style.opacity = "1";
+      document.getElementById("s4").style.opacity = "1";
+      document.getElementById("menu").classList.toggle("visible", false);
       this.open = !this.open;
     }else{
-      document.getElementById("stripe2").style.transform = "rotate(135deg)";
-      document.getElementById("stripe4").style.transform = "rotate(-135deg)";
-      document.getElementById("stripe1").style.opacity = "0";
-      document.getElementById("stripe3").style.opacity = "0";
+      document.getElementById("s2").style.transform = "rotate(135deg)";
+      document.getElementById("s3").style.transform = "rotate(-135deg)";
+      document.getElementById("s1").style.opacity = "0";
+      document.getElementById("s4").style.opacity = "0";
       document.getElementById("menu").classList.toggle("visible", true);
-
       this.open = !this.open;
     }
-
-
-    
-    
   }
 }
--- src/stories/hamburger-menu.stories.ts
+++ src/stories/hamburger-menu.stories.ts
@@ -1,5 +1,4 @@
 import { storiesOf } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
 import { HamburgerMenuComponent } from '../app/hamburger-menu/hamburger-menu.component';
 import * as markdown from './notes/hamburger.notes.md';
 

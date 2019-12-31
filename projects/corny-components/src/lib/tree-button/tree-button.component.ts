466efe2e4b63e3dd5062f6e35ce698ffb896207c
kl4532 <kornellukasiak@gmail.com>
2019-12-31 09:57:16 +0100

    added mock data categories in tree

--- projects/corny-components/src/lib/tree-button/tree-button.component.ts
+++ projects/corny-components/src/lib/tree-button/tree-button.component.ts
@@ -7,13 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeButtonComponent implements OnInit {
 
+  open: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
-
-  toggle() {
-
+  toggle(i) {
+    const btn = document.getElementsByTagName('button')[i];
+    this.open = !this.open;
+    this.open ? btn.innerText = "-" : btn.innerText = "+"; 
   }
+
+ categories = [
+      { 
+        "id": "1",
+        "name": "cat1-start", 
+        "subcategory": [{
+          "id": "1.1",
+          "name": "sub1",
+          "subcategory": [{
+            "id": "1.1.1",
+            "name": "sub1.1",
+            "subcategory": [{
+              "id": "1.1.1.1",
+              "name": "sub1.1.1-last-child"
+            }]
+          }]
+        }]
+      },
+  
+      { 
+        "id": "2",
+        "name": "cat2", 
+        "subcategory": [
+        {
+          "id": "2.1",
+          "name": "sub1"
+        },
+        {
+          "id": "2.2",
+          "name": "sub2"
+        },
+        {
+          "id": "2.3",
+          "name": "sub3"
+        }
+        ]
+      },
+      
+      { 
+        "id": "3",
+        "name": "cat3"
+      },
+      { 
+        "id": "4",
+        "name": "cat4"
+      },
+      { 
+        "id": "5",
+        "name": "cat5"
+      },
+      { 
+        "id": "6",
+        "name": "cat6"
+      }
+  ]
+
 }

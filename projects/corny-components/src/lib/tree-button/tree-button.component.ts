e513f721d445204ad79bc87b1165b55fe5c3ad2e
kl4532 <kornellukasiak@gmail.com>
2020-01-05 12:23:28 +0100

    tree component used recursively

--- projects/corny-components/src/lib/tree-button/tree-button.component.ts
+++ projects/corny-components/src/lib/tree-button/tree-button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HideLinkComponent } from '../hide-link/hide-link.component';
 
 @Component({
   selector: 'tree-button',
@@ -7,71 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeButtonComponent implements OnInit {
 
-  open: boolean = false;
   constructor() { }
-
+  // open = false;
+  @Input() categories;
   ngOnInit() {
   }
 
-  toggle(i) {
+  toggle(category, i) {
     const btn = document.getElementsByTagName('button')[i];
-    this.open = !this.open;
-    this.open ? btn.innerText = "-" : btn.innerText = "+"; 
+    // if(!this.open){
+    //   btn.innerHTML = "-";
+    //   this.open = true;
+    // }else {
+    //   btn.innerHTML = "+";
+    //   this.open = false;  
+    // }
+    // open ? btn.innerHTML = "-" : btn.innerHTML = "+"; 
   }
-
- categories = [
-      { 
-        "id": "1",
-        "name": "cat1-start", 
-        "subcategory": [{
-          "id": "1.1",
-          "name": "sub1",
-          "subcategory": [{
-            "id": "1.1.1",
-            "name": "sub1.1",
-            "subcategory": [{
-              "id": "1.1.1.1",
-              "name": "sub1.1.1-last-child"
-            }]
-          }]
-        }]
-      },
-  
-      { 
-        "id": "2",
-        "name": "cat2", 
-        "subcategory": [
-        {
-          "id": "2.1",
-          "name": "sub1"
-        },
-        {
-          "id": "2.2",
-          "name": "sub2"
-        },
-        {
-          "id": "2.3",
-          "name": "sub3"
-        }
-        ]
-      },
-      
-      { 
-        "id": "3",
-        "name": "cat3"
-      },
-      { 
-        "id": "4",
-        "name": "cat4"
-      },
-      { 
-        "id": "5",
-        "name": "cat5"
-      },
-      { 
-        "id": "6",
-        "name": "cat6"
-      }
-  ]
 
 }

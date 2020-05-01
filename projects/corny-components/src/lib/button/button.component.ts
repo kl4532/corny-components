bb97474afef17cc40f9278ed792bcdf50d468d59
kl4532 <kornellukasiak@gmail.com>
2020-05-01 11:02:15 +0200

    added focus and active state styling

--- projects/corny-components/src/lib/button/button.component.ts
+++ projects/corny-components/src/lib/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input  } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, HostListener, Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'c-button',
@@ -10,12 +10,39 @@ export class ButtonComponent implements OnInit {
   @Input() text: string;
   @Input() disabled: boolean = false;
 
-  constructor() {}
+  constructor( private rend: Renderer2, private el: ElementRef) {}
 
   ngOnInit() {
     this.text = this.text || this.btnType;
   }
 
-  generic() {}
+  @HostListener('mousedown', ['$event']) 
+    triggerMouseClick() {
+      this.toggleActive(true);
+    }
+  @HostListener('keydown', ['$event']) 
+    triggerActive(event: KeyboardEvent) {
+      if(event.key === "Enter" || event.key === " ") {
+        this.toggleActive(true);
+      }
+    }
+  @HostListener('mouseup', ['$event']) 
+    deactivateMouse() {
+      this.toggleActive(false);
+    }
+  @HostListener('keyup', ['$event'])
+    triggerInactive(event: KeyboardEvent) {
+      if(event.key === "Enter" || event.key === " ") {
+        this.toggleActive(false);
+      }
+  } 
 
+  toggleActive(flag: boolean) {
+    if(flag) {
+      this.rend.addClass(this.el.nativeElement.children[0], 'activated');
+    } else {
+      this.rend.removeClass(this.el.nativeElement.children[0], 'activated');
+    }
+  }
+  
 }

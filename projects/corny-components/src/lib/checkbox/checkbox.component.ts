574b33aeefdeb63fb6992f775fffb69007ef0d2d
kl4532 <kornellukasiak@gmail.com>
2020-05-03 13:46:57 +0200

    checkbox component init

---
+++ projects/corny-components/src/lib/checkbox/checkbox.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'c-checkbox',
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.scss']
+})
+export class CheckboxComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+  
+}
--- projects/corny-components/src/public-api.ts
+++ projects/corny-components/src/public-api.ts
@@ -9,6 +9,8 @@ export * from './lib/button/button.component';
 export * from './lib/searchbar/searchbar.component';
 export * from './lib/hide-link/hide-link.component';
 export * from './lib/tree-button/tree-button.component';
+//AddedByScript
+export * from './lib/checkbox/checkbox.component';
 
 
 

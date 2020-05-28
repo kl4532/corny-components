ba123109660fe3fcb646a791e88e39e787f8a9ce
kl4532 <kornellukasiak@gmail.com>
2020-05-28 12:19:10 +0200

    keyboard navigation

--- projects/corny-components/src/lib/components/dropdown/dropdown.component.ts
+++ projects/corny-components/src/lib/components/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'c-dropdown',
@@ -9,11 +9,52 @@ export class DropdownComponent implements OnInit {
 
   @Input() options = [];
   @Input() filter = false;
-  selected = "--select";
+  selected = "-- select";
   open = false;
-  
-  constructor() {}
+  childIndex=0;
+  currentChild: HTMLElement;
+  list: any;
 
-  ngOnInit() {}
+  
+  constructor(private element: ElementRef) {
+  }
+
+  ngOnInit() {
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  keyNavigation(event: KeyboardEvent) {
+    this.list = this.element.nativeElement.querySelector("ul");
+    if (event.key === "ArrowDown") {
+      if(this.childIndex < this.options.length) {
+        this.selected = this.options[this.childIndex];
+        if(this.open) {
+          this.list.children[this.childIndex].focus();
+        }
+        this.childIndex === this.options.length-1 ? null : this.childIndex++;
+        console.log(this.childIndex);
+      }
+    }
+
+    if (event.key === "ArrowUp") {
+      if(this.childIndex > 0) {
+        this.childIndex--;
+        this.selected = this.options[this.childIndex];
+        if(this.open) {
+          this.list.children[this.childIndex].focus();
+        }
+      }
+    }
+
+    if (event.key === "Escape") {
+      this.open = false;
+      console.log("Esc pushed");
+    }
+
+    if (event.key === " ") {
+      this.open = true;
+      console.log("space pushed");
+    }
+  }
   
 }

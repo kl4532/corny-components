8b19ec8982c83de4432440ed2f9a2e5117e328a2
kl4532 <kornellukasiak@gmail.com>
2020-07-18 13:28:29 +0200

    modify focused directive

--- projects/corny-components/src/lib/directives/focused.directive.ts
+++ projects/corny-components/src/lib/directives/focused.directive.ts
@@ -1,16 +1,30 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, Renderer2, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[c-focused]'
 })
 export class FocusedDirective {
 
-  constructor() { }
+  constructor(private rend: Renderer2, private el: ElementRef) { }
 
   @HostBinding('style.color') color: string;
   @HostBinding('style.border-color') borderColor: string;
-  @HostListener('focus') onFocus(){
+
+  @HostListener('keyup', ['$event']) 
+    triggerActive(event: KeyboardEvent) {
+      if(event.key === "Enter" || event.key === " ") {
+        this.toggleActive(true);
+      }
+    }
     
+  toggleActive(flag: boolean) {
+    if(flag) {
+      this.el.nativeElement.click();
+      // this.rend.addClass(this.el.nativeElement.children[0], 'activated');
+    } else {
+      this.el.nativeElement.click();
+      // this.rend.removeClass(this.el.nativeElement.children[0], 'activated');
+    }
   }
 
 }
--- projects/corny-components/src/public-api.ts
+++ projects/corny-components/src/public-api.ts
@@ -2,6 +2,10 @@
  * Public API Surface of corny-components
  */
 
+ // Directives
+export * from './lib/directives/focused.directive';
+
+ //Components
 export * from './lib/corny-components.component';
 export * from './lib/corny-components.module';
 
@@ -9,9 +13,12 @@ export * from './lib/components/button/button.component';
 export * from './lib/components/searchbar/searchbar.component';
 export * from './lib/components/hide-link/hide-link.component';
 export * from './lib/components/tree-button/tree-button.component';
+
 //AddedByScript
 export * from './lib/components/dropdown/dropdown.component';
 export * from './lib/components/checkbox/checkbox.component';
+
+
 
 
 

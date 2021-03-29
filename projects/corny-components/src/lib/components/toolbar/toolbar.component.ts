ad7d4c1045a603cd7712c05c5ba6d9419c5cb2f0
kl4532 <kornellukasiak@gmail.com>
2021-03-29 21:00:34 +0200

    add toolbar component

---
+++ projects/corny-components/src/lib/components/toolbar/toolbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'c-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
